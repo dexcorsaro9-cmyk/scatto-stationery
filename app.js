68276b6e4dfb599da749db50dc5b629573c39fc3
dexcorsaro9-cmyk <gamepassion9@gmail.com>
2026-07-21 01:35:44 +0200

    app.js

--- app.js
+++ app.js
@@ -1,7 +1,904 @@
 let levels = [
-  { level: 1, targetPens: 8, targetNotes: 6, targetAccessories: 5, spawnInterval: 1.1, beltSpeed: 1, allowedMistakes: 5 },
-  { level: 2, targetPens: 10, targetNotes: 8, targetAccessories: 6, spawnInterval: 1.0, beltSpeed: 1.15, allowedMistakes: 5 },
-  { level: 3, targetPens: 12, targetNotes: 10, targetAccessories: 8, spawnInterval: 0.9, beltSpeed: 1.3, allowedMistakes: 4 }
+  {
+    "level": 1,
+    "targetPens": 6,
+    "targetNotes": 4,
+    "targetAccessories": 3,
+    "spawnInterval": 1.143,
+    "beltSpeed": 1.012,
+    "allowedMistakes": 6
+  },
+  {
+    "level": 2,
+    "targetPens": 6,
+    "targetNotes": 4,
+    "targetAccessories": 3,
+    "spawnInterval": 1.136,
+    "beltSpeed": 1.024,
+    "allowedMistakes": 6
+  },
+  {
+    "level": 3,
+    "targetPens": 7,
+    "targetNotes": 4,
+    "targetAccessories": 3,
+    "spawnInterval": 1.129,
+    "beltSpeed": 1.036,
+    "allowedMistakes": 6
+  },
+  {
+    "level": 4,
+    "targetPens": 7,
+    "targetNotes": 5,
+    "targetAccessories": 3,
+    "spawnInterval": 1.122,
+    "beltSpeed": 1.048,
+    "allowedMistakes": 6
+  },
+  {
+    "level": 5,
+    "targetPens": 7,
+    "targetNotes": 5,
+    "targetAccessories": 4,
+    "spawnInterval": 1.115,
+    "beltSpeed": 1.06,
+    "allowedMistakes": 6
+  },
+  {
+    "level": 6,
+    "targetPens": 8,
+    "targetNotes": 5,
+    "targetAccessories": 4,
+    "spawnInterval": 1.108,
+    "beltSpeed": 1.072,
+    "allowedMistakes": 6
+  },
+  {
+    "level": 7,
+    "targetPens": 8,
+    "targetNotes": 5,
+    "targetAccessories": 4,
+    "spawnInterval": 1.101,
+    "beltSpeed": 1.084,
+    "allowedMistakes": 6
+  },
+  {
+    "level": 8,
+    "targetPens": 8,
+    "targetNotes": 6,
+    "targetAccessories": 4,
+    "spawnInterval": 1.094,
+    "beltSpeed": 1.096,
+    "allowedMistakes": 6
+  },
+  {
+    "level": 9,
+    "targetPens": 9,
+    "targetNotes": 6,
+    "targetAccessories": 4,
+    "spawnInterval": 1.087,
+    "beltSpeed": 1.108,
+    "allowedMistakes": 6
+  },
+  {
+    "level": 10,
+    "targetPens": 9,
+    "targetNotes": 6,
+    "targetAccessories": 5,
+    "spawnInterval": 1.08,
+    "beltSpeed": 1.12,
+    "allowedMistakes": 6
+  },
+  {
+    "level": 11,
+    "targetPens": 9,
+    "targetNotes": 6,
+    "targetAccessories": 5,
+    "spawnInterval": 1.073,
+    "beltSpeed": 1.132,
+    "allowedMistakes": 6
+  },
+  {
+    "level": 12,
+    "targetPens": 10,
+    "targetNotes": 7,
+    "targetAccessories": 5,
+    "spawnInterval": 1.066,
+    "beltSpeed": 1.144,
+    "allowedMistakes": 6
+  },
+  {
+    "level": 13,
+    "targetPens": 10,
+    "targetNotes": 7,
+    "targetAccessories": 5,
+    "spawnInterval": 1.059,
+    "beltSpeed": 1.156,
+    "allowedMistakes": 6
+  },
+  {
+    "level": 14,
+    "targetPens": 10,
+    "targetNotes": 7,
+    "targetAccessories": 5,
+    "spawnInterval": 1.052,
+    "beltSpeed": 1.168,
+    "allowedMistakes": 6
+  },
+  {
+    "level": 15,
+    "targetPens": 11,
+    "targetNotes": 7,
+    "targetAccessories": 6,
+    "spawnInterval": 1.045,
+    "beltSpeed": 1.18,
+    "allowedMistakes": 6
+  },
+  {
+    "level": 16,
+    "targetPens": 11,
+    "targetNotes": 8,
+    "targetAccessories": 6,
+    "spawnInterval": 1.038,
+    "beltSpeed": 1.192,
+    "allowedMistakes": 6
+  },
+  {
+    "level": 17,
+    "targetPens": 11,
+    "targetNotes": 8,
+    "targetAccessories": 6,
+    "spawnInterval": 1.031,
+    "beltSpeed": 1.204,
+    "allowedMistakes": 6
+  },
+  {
+    "level": 18,
+    "targetPens": 12,
+    "targetNotes": 8,
+    "targetAccessories": 6,
+    "spawnInterval": 1.024,
+    "beltSpeed": 1.216,
+    "allowedMistakes": 6
+  },
+  {
+    "level": 19,
+    "targetPens": 12,
+    "targetNotes": 8,
+    "targetAccessories": 6,
+    "spawnInterval": 1.017,
+    "beltSpeed": 1.228,
+    "allowedMistakes": 6
+  },
+  {
+    "level": 20,
+    "targetPens": 12,
+    "targetNotes": 9,
+    "targetAccessories": 7,
+    "spawnInterval": 1.01,
+    "beltSpeed": 1.24,
+    "allowedMistakes": 5
+  },
+  {
+    "level": 21,
+    "targetPens": 13,
+    "targetNotes": 9,
+    "targetAccessories": 7,
+    "spawnInterval": 1.003,
+    "beltSpeed": 1.252,
+    "allowedMistakes": 5
+  },
+  {
+    "level": 22,
+    "targetPens": 13,
+    "targetNotes": 9,
+    "targetAccessories": 7,
+    "spawnInterval": 0.996,
+    "beltSpeed": 1.264,
+    "allowedMistakes": 5
+  },
+  {
+    "level": 23,
+    "targetPens": 13,
+    "targetNotes": 9,
+    "targetAccessories": 7,
+    "spawnInterval": 0.989,
+    "beltSpeed": 1.276,
+    "allowedMistakes": 5
+  },
+  {
+    "level": 24,
+    "targetPens": 14,
+    "targetNotes": 10,
+    "targetAccessories": 7,
+    "spawnInterval": 0.982,
+    "beltSpeed": 1.288,
+    "allowedMistakes": 5
+  },
+  {
+    "level": 25,
+    "targetPens": 14,
+    "targetNotes": 10,
+    "targetAccessories": 8,
+    "spawnInterval": 0.975,
+    "beltSpeed": 1.3,
+    "allowedMistakes": 5
+  },
+  {
+    "level": 26,
+    "targetPens": 14,
+    "targetNotes": 10,
+    "targetAccessories": 8,
+    "spawnInterval": 0.968,
+    "beltSpeed": 1.312,
+    "allowedMistakes": 5
+  },
+  {
+    "level": 27,
+    "targetPens": 15,
+    "targetNotes": 10,
+    "targetAccessories": 8,
+    "spawnInterval": 0.961,
+    "beltSpeed": 1.324,
+    "allowedMistakes": 5
+  },
+  {
+    "level": 28,
+    "targetPens": 15,
+    "targetNotes": 11,
+    "targetAccessories": 8,
+    "spawnInterval": 0.954,
+    "beltSpeed": 1.336,
+    "allowedMistakes": 5
+  },
+  {
+    "level": 29,
+    "targetPens": 15,
+    "targetNotes": 11,
+    "targetAccessories": 8,
+    "spawnInterval": 0.947,
+    "beltSpeed": 1.348,
+    "allowedMistakes": 5
+  },
+  {
+    "level": 30,
+    "targetPens": 16,
+    "targetNotes": 11,
+    "targetAccessories": 9,
+    "spawnInterval": 0.94,
+    "beltSpeed": 1.36,
+    "allowedMistakes": 5
+  },
+  {
+    "level": 31,
+    "targetPens": 16,
+    "targetNotes": 11,
+    "targetAccessories": 9,
+    "spawnInterval": 0.933,
+    "beltSpeed": 1.372,
+    "allowedMistakes": 5
+  },
+  {
+    "level": 32,
+    "targetPens": 16,
+    "targetNotes": 12,
+    "targetAccessories": 9,
+    "spawnInterval": 0.926,
+    "beltSpeed": 1.384,
+    "allowedMistakes": 5
+  },
+  {
+    "level": 33,
+    "targetPens": 17,
+    "targetNotes": 12,
+    "targetAccessories": 9,
+    "spawnInterval": 0.919,
+    "beltSpeed": 1.396,
+    "allowedMistakes": 5
+  },
+  {
+    "level": 34,
+    "targetPens": 17,
+    "targetNotes": 12,
+    "targetAccessories": 9,
+    "spawnInterval": 0.912,
+    "beltSpeed": 1.408,
+    "allowedMistakes": 5
+  },
+  {
+    "level": 35,
+    "targetPens": 17,
+    "targetNotes": 12,
+    "targetAccessories": 10,
+    "spawnInterval": 0.905,
+    "beltSpeed": 1.42,
+    "allowedMistakes": 5
+  },
+  {
+    "level": 36,
+    "targetPens": 18,
+    "targetNotes": 13,
+    "targetAccessories": 10,
+    "spawnInterval": 0.898,
+    "beltSpeed": 1.432,
+    "allowedMistakes": 5
+  },
+  {
+    "level": 37,
+    "targetPens": 18,
+    "targetNotes": 13,
+    "targetAccessories": 10,
+    "spawnInterval": 0.891,
+    "beltSpeed": 1.444,
+    "allowedMistakes": 5
+  },
+  {
+    "level": 38,
+    "targetPens": 18,
+    "targetNotes": 13,
+    "targetAccessories": 10,
+    "spawnInterval": 0.884,
+    "beltSpeed": 1.456,
+    "allowedMistakes": 5
+  },
+  {
+    "level": 39,
+    "targetPens": 19,
+    "targetNotes": 13,
+    "targetAccessories": 10,
+    "spawnInterval": 0.877,
+    "beltSpeed": 1.468,
+    "allowedMistakes": 5
+  },
+  {
+    "level": 40,
+    "targetPens": 19,
+    "targetNotes": 14,
+    "targetAccessories": 11,
+    "spawnInterval": 0.87,
+    "beltSpeed": 1.48,
+    "allowedMistakes": 4
+  },
+  {
+    "level": 41,
+    "targetPens": 19,
+    "targetNotes": 14,
+    "targetAccessories": 11,
+    "spawnInterval": 0.863,
+    "beltSpeed": 1.492,
+    "allowedMistakes": 4
+  },
+  {
+    "level": 42,
+    "targetPens": 20,
+    "targetNotes": 14,
+    "targetAccessories": 11,
+    "spawnInterval": 0.856,
+    "beltSpeed": 1.504,
+    "allowedMistakes": 4
+  },
+  {
+    "level": 43,
+    "targetPens": 20,
+    "targetNotes": 14,
+    "targetAccessories": 11,
+    "spawnInterval": 0.849,
+    "beltSpeed": 1.516,
+    "allowedMistakes": 4
+  },
+  {
+    "level": 44,
+    "targetPens": 20,
+    "targetNotes": 15,
+    "targetAccessories": 11,
+    "spawnInterval": 0.842,
+    "beltSpeed": 1.528,
+    "allowedMistakes": 4
+  },
+  {
+    "level": 45,
+    "targetPens": 21,
+    "targetNotes": 15,
+    "targetAccessories": 12,
+    "spawnInterval": 0.835,
+    "beltSpeed": 1.54,
+    "allowedMistakes": 4
+  },
+  {
+    "level": 46,
+    "targetPens": 21,
+    "targetNotes": 15,
+    "targetAccessories": 12,
+    "spawnInterval": 0.828,
+    "beltSpeed": 1.552,
+    "allowedMistakes": 4
+  },
+  {
+    "level": 47,
+    "targetPens": 21,
+    "targetNotes": 15,
+    "targetAccessories": 12,
+    "spawnInterval": 0.821,
+    "beltSpeed": 1.564,
+    "allowedMistakes": 4
+  },
+  {
+    "level": 48,
+    "targetPens": 22,
+    "targetNotes": 16,
+    "targetAccessories": 12,
+    "spawnInterval": 0.814,
+    "beltSpeed": 1.576,
+    "allowedMistakes": 4
+  },
+  {
+    "level": 49,
+    "targetPens": 22,
+    "targetNotes": 16,
+    "targetAccessories": 12,
+    "spawnInterval": 0.807,
+    "beltSpeed": 1.588,
+    "allowedMistakes": 4
+  },
+  {
+    "level": 50,
+    "targetPens": 22,
+    "targetNotes": 16,
+    "targetAccessories": 13,
+    "spawnInterval": 0.8,
+    "beltSpeed": 1.6,
+    "allowedMistakes": 4
+  },
+  {
+    "level": 51,
+    "targetPens": 23,
+    "targetNotes": 16,
+    "targetAccessories": 13,
+    "spawnInterval": 0.793,
+    "beltSpeed": 1.612,
+    "allowedMistakes": 4
+  },
+  {
+    "level": 52,
+    "targetPens": 23,
+    "targetNotes": 17,
+    "targetAccessories": 13,
+    "spawnInterval": 0.786,
+    "beltSpeed": 1.624,
+    "allowedMistakes": 4
+  },
+  {
+    "level": 53,
+    "targetPens": 23,
+    "targetNotes": 17,
+    "targetAccessories": 13,
+    "spawnInterval": 0.779,
+    "beltSpeed": 1.636,
+    "allowedMistakes": 4
+  },
+  {
+    "level": 54,
+    "targetPens": 24,
+    "targetNotes": 17,
+    "targetAccessories": 13,
+    "spawnInterval": 0.772,
+    "beltSpeed": 1.648,
+    "allowedMistakes": 4
+  },
+  {
+    "level": 55,
+    "targetPens": 24,
+    "targetNotes": 17,
+    "targetAccessories": 14,
+    "spawnInterval": 0.765,
+    "beltSpeed": 1.66,
+    "allowedMistakes": 4
+  },
+  {
+    "level": 56,
+    "targetPens": 24,
+    "targetNotes": 18,
+    "targetAccessories": 14,
+    "spawnInterval": 0.758,
+    "beltSpeed": 1.672,
+    "allowedMistakes": 4
+  },
+  {
+    "level": 57,
+    "targetPens": 25,
+    "targetNotes": 18,
+    "targetAccessories": 14,
+    "spawnInterval": 0.751,
+    "beltSpeed": 1.684,
+    "allowedMistakes": 4
+  },
+  {
+    "level": 58,
+    "targetPens": 25,
+    "targetNotes": 18,
+    "targetAccessories": 14,
+    "spawnInterval": 0.744,
+    "beltSpeed": 1.696,
+    "allowedMistakes": 4
+  },
+  {
+    "level": 59,
+    "targetPens": 25,
+    "targetNotes": 18,
+    "targetAccessories": 14,
+    "spawnInterval": 0.737,
+    "beltSpeed": 1.708,
+    "allowedMistakes": 4
+  },
+  {
+    "level": 60,
+    "targetPens": 26,
+    "targetNotes": 19,
+    "targetAccessories": 15,
+    "spawnInterval": 0.73,
+    "beltSpeed": 1.72,
+    "allowedMistakes": 3
+  },
+  {
+    "level": 61,
+    "targetPens": 26,
+    "targetNotes": 19,
+    "targetAccessories": 15,
+    "spawnInterval": 0.723,
+    "beltSpeed": 1.732,
+    "allowedMistakes": 3
+  },
+  {
+    "level": 62,
+    "targetPens": 26,
+    "targetNotes": 19,
+    "targetAccessories": 15,
+    "spawnInterval": 0.716,
+    "beltSpeed": 1.744,
+    "allowedMistakes": 3
+  },
+  {
+    "level": 63,
+    "targetPens": 27,
+    "targetNotes": 19,
+    "targetAccessories": 15,
+    "spawnInterval": 0.709,
+    "beltSpeed": 1.756,
+    "allowedMistakes": 3
+  },
+  {
+    "level": 64,
+    "targetPens": 27,
+    "targetNotes": 20,
+    "targetAccessories": 15,
+    "spawnInterval": 0.702,
+    "beltSpeed": 1.768,
+    "allowedMistakes": 3
+  },
+  {
+    "level": 65,
+    "targetPens": 27,
+    "targetNotes": 20,
+    "targetAccessories": 16,
+    "spawnInterval": 0.695,
+    "beltSpeed": 1.78,
+    "allowedMistakes": 3
+  },
+  {
+    "level": 66,
+    "targetPens": 28,
+    "targetNotes": 20,
+    "targetAccessories": 16,
+    "spawnInterval": 0.688,
+    "beltSpeed": 1.792,
+    "allowedMistakes": 3
+  },
+  {
+    "level": 67,
+    "targetPens": 28,
+    "targetNotes": 20,
+    "targetAccessories": 16,
+    "spawnInterval": 0.681,
+    "beltSpeed": 1.804,
+    "allowedMistakes": 3
+  },
+  {
+    "level": 68,
+    "targetPens": 28,
+    "targetNotes": 21,
+    "targetAccessories": 16,
+    "spawnInterval": 0.674,
+    "beltSpeed": 1.816,
+    "allowedMistakes": 3
+  },
+  {
+    "level": 69,
+    "targetPens": 29,
+    "targetNotes": 21,
+    "targetAccessories": 16,
+    "spawnInterval": 0.667,
+    "beltSpeed": 1.828,
+    "allowedMistakes": 3
+  },
+  {
+    "level": 70,
+    "targetPens": 29,
+    "targetNotes": 21,
+    "targetAccessories": 17,
+    "spawnInterval": 0.66,
+    "beltSpeed": 1.84,
+    "allowedMistakes": 3
+  },
+  {
+    "level": 71,
+    "targetPens": 29,
+    "targetNotes": 21,
+    "targetAccessories": 17,
+    "spawnInterval": 0.653,
+    "beltSpeed": 1.852,
+    "allowedMistakes": 3
+  },
+  {
+    "level": 72,
+    "targetPens": 30,
+    "targetNotes": 22,
+    "targetAccessories": 17,
+    "spawnInterval": 0.646,
+    "beltSpeed": 1.864,
+    "allowedMistakes": 3
+  },
+  {
+    "level": 73,
+    "targetPens": 30,
+    "targetNotes": 22,
+    "targetAccessories": 17,
+    "spawnInterval": 0.639,
+    "beltSpeed": 1.876,
+    "allowedMistakes": 3
+  },
+  {
+    "level": 74,
+    "targetPens": 30,
+    "targetNotes": 22,
+    "targetAccessories": 17,
+    "spawnInterval": 0.632,
+    "beltSpeed": 1.888,
+    "allowedMistakes": 3
+  },
+  {
+    "level": 75,
+    "targetPens": 30,
+    "targetNotes": 22,
+    "targetAccessories": 18,
+    "spawnInterval": 0.625,
+    "beltSpeed": 1.9,
+    "allowedMistakes": 3
+  },
+  {
+    "level": 76,
+    "targetPens": 30,
+    "targetNotes": 23,
+    "targetAccessories": 18,
+    "spawnInterval": 0.618,
+    "beltSpeed": 1.912,
+    "allowedMistakes": 3
+  },
+  {
+    "level": 77,
+    "targetPens": 30,
+    "targetNotes": 23,
+    "targetAccessories": 18,
+    "spawnInterval": 0.611,
+    "beltSpeed": 1.924,
+    "allowedMistakes": 3
+  },
+  {
+    "level": 78,
+    "targetPens": 30,
+    "targetNotes": 23,
+    "targetAccessories": 18,
+    "spawnInterval": 0.604,
+    "beltSpeed": 1.936,
+    "allowedMistakes": 3
+  },
+  {
+    "level": 79,
+    "targetPens": 30,
+    "targetNotes": 23,
+    "targetAccessories": 18,
+    "spawnInterval": 0.597,
+    "beltSpeed": 1.948,
+    "allowedMistakes": 3
+  },
+  {
+    "level": 80,
+    "targetPens": 30,
+    "targetNotes": 24,
+    "targetAccessories": 19,
+    "spawnInterval": 0.59,
+    "beltSpeed": 1.96,
+    "allowedMistakes": 2
+  },
+  {
+    "level": 81,
+    "targetPens": 30,
+    "targetNotes": 24,
+    "targetAccessories": 19,
+    "spawnInterval": 0.583,
+    "beltSpeed": 1.972,
+    "allowedMistakes": 2
+  },
+  {
+    "level": 82,
+    "targetPens": 30,
+    "targetNotes": 24,
+    "targetAccessories": 19,
+    "spawnInterval": 0.576,
+    "beltSpeed": 1.984,
+    "allowedMistakes": 2
+  },
+  {
+    "level": 83,
+    "targetPens": 30,
+    "targetNotes": 24,
+    "targetAccessories": 19,
+    "spawnInterval": 0.569,
+    "beltSpeed": 1.996,
+    "allowedMistakes": 2
+  },
+  {
+    "level": 84,
+    "targetPens": 30,
+    "targetNotes": 24,
+    "targetAccessories": 19,
+    "spawnInterval": 0.562,
+    "beltSpeed": 2.008,
+    "allowedMistakes": 2
+  },
+  {
+    "level": 85,
+    "targetPens": 30,
+    "targetNotes": 24,
+    "targetAccessories": 20,
+    "spawnInterval": 0.555,
+    "beltSpeed": 2.02,
+    "allowedMistakes": 2
+  },
+  {
+    "level": 86,
+    "targetPens": 30,
+    "targetNotes": 24,
+    "targetAccessories": 20,
+    "spawnInterval": 0.548,
+    "beltSpeed": 2.032,
+    "allowedMistakes": 2
+  },
+  {
+    "level": 87,
+    "targetPens": 30,
+    "targetNotes": 24,
+    "targetAccessories": 20,
+    "spawnInterval": 0.541,
+    "beltSpeed": 2.044,
+    "allowedMistakes": 2
+  },
+  {
+    "level": 88,
+    "targetPens": 30,
+    "targetNotes": 24,
+    "targetAccessories": 20,
+    "spawnInterval": 0.534,
+    "beltSpeed": 2.056,
+    "allowedMistakes": 2
+  },
+  {
+    "level": 89,
+    "targetPens": 30,
+    "targetNotes": 24,
+    "targetAccessories": 20,
+    "spawnInterval": 0.527,
+    "beltSpeed": 2.068,
+    "allowedMistakes": 2
+  },
+  {
+    "level": 90,
+    "targetPens": 30,
+    "targetNotes": 24,
+    "targetAccessories": 20,
+    "spawnInterval": 0.52,
+    "beltSpeed": 2.08,
+    "allowedMistakes": 2
+  },
+  {
+    "level": 91,
+    "targetPens": 30,
+    "targetNotes": 24,
+    "targetAccessories": 20,
+    "spawnInterval": 0.513,
+    "beltSpeed": 2.092,
+    "allowedMistakes": 2
+  },
+  {
+    "level": 92,
+    "targetPens": 30,
+    "targetNotes": 24,
+    "targetAccessories": 20,
+    "spawnInterval": 0.506,
+    "beltSpeed": 2.104,
+    "allowedMistakes": 2
+  },
+  {
+    "level": 93,
+    "targetPens": 30,
+    "targetNotes": 24,
+    "targetAccessories": 20,
+    "spawnInterval": 0.499,
+    "beltSpeed": 2.116,
+    "allowedMistakes": 2
+  },
+  {
+    "level": 94,
+    "targetPens": 30,
+    "targetNotes": 24,
+    "targetAccessories": 20,
+    "spawnInterval": 0.492,
+    "beltSpeed": 2.128,
+    "allowedMistakes": 2
+  },
+  {
+    "level": 95,
+    "targetPens": 30,
+    "targetNotes": 24,
+    "targetAccessories": 20,
+    "spawnInterval": 0.485,
+    "beltSpeed": 2.14,
+    "allowedMistakes": 2
+  },
+  {
+    "level": 96,
+    "targetPens": 30,
+    "targetNotes": 24,
+    "targetAccessories": 20,
+    "spawnInterval": 0.478,
+    "beltSpeed": 2.152,
+    "allowedMistakes": 2
+  },
+  {
+    "level": 97,
+    "targetPens": 30,
+    "targetNotes": 24,
+    "targetAccessories": 20,
+    "spawnInterval": 0.471,
+    "beltSpeed": 2.164,
+    "allowedMistakes": 2
+  },
+  {
+    "level": 98,
+    "targetPens": 30,
+    "targetNotes": 24,
+    "targetAccessories": 20,
+    "spawnInterval": 0.464,
+    "beltSpeed": 2.176,
+    "allowedMistakes": 2
+  },
+  {
+    "level": 99,
+    "targetPens": 30,
+    "targetNotes": 24,
+    "targetAccessories": 20,
+    "spawnInterval": 0.457,
+    "beltSpeed": 2.188,
+    "allowedMistakes": 2
+  },
+  {
+    "level": 100,
+    "targetPens": 30,
+    "targetNotes": 24,
+    "targetAccessories": 20,
+    "spawnInterval": 0.45,
+    "beltSpeed": 2.2,
+    "allowedMistakes": 2
+  }
 ];
 
 let currentLevelIndex = 0;
